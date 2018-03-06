ac30d0f6bd0298e3b05763db3ec6121e533b1460
LeezaLuncheon Luncheon <leezaluncheon@gmail.com>
2018-03-06 03:34:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 function add(a,b){
   return a+b;
 }
+function subtraact(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function divide(a,b){
+  return a/b;
+}
+function inc(n){
+  return n++;
+}
+function dec(n){
+  return n--;
+}
+function makeInt(n){
+  return parseInt(n);
+}
+function preserveDecimal(n){
+  return parseFloat(n);
+}
+
